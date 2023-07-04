f9d3d094484333aacbce860960a9e27fd6a7b1e5
EriclesRibeiro <ericles00@hotmail.com>
2023-07-03 21:21:57 -0300

    feat: getusers with mongodb

--- src/repositories/get-users/getUsers.repository.ts
+++ src/repositories/get-users/getUsers.repository.ts
@@ -1,16 +1,12 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import MongoClient from "../../database/mongo";
 import User from "../../models/user.model";
 
 class GetUsersRepository implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
-        return [
-            {
-                email: 'fdec',
-                firstName: 'cdccdc',
-                lastName: 'cc',
-                password: 'cdc'
-            }
-        ]
+
+        const users = await MongoClient.db.collection<Omit<User, "id">>('users').find({}).toArray()
+        return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }))
     }
 
 }
